17fb5be52feda6a617587b899ec9773a310a4431
Moncayo1329 <86531347+Moncayo1329@users.noreply.github.com>
2023-07-25 17:46:59 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
+import React from 'react'; // Importante agregar esta línea para usar React
 import logo from './logo.svg';
 import './App.css';
 
+
+
+function Header (){
+return (
+
+  <header>
+
+<nav className="Nav"> 
+<img src={logo} className="nav-logo" alt="Logo" />
+      <h1>Fun facts about React</h1>
+      <ul className="Nav-Items">
+    <li>Pricing</li>
+    <li>About</li>
+    <li>Contact</li>
+
+
+      </ul>
+      
+       </nav>
+</header>
+)
+}
+
+function Footer() {
+
+  return (
+     
+    <footer><small> @ 2021 All right reserved</small></footer>
+
+    )
+  } 
+
+  function MainContent() {
+
+    return (
+       <div>
+      <ol>
+
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>has well over 100k starts on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+      
+      
+      
+          </ol>
+          </div>
+      )
+    }
+
+
+
+
+
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+   <div>
+
+<Header />
+
+<MainContent />
+
+<Footer />
+
     </div>
   );
 }
